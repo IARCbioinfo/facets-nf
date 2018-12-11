47f138a2e683b6b4be2c23d47f9f75dfd608ac8f
V-Catherine <voegelec@iarc.fr>
2018-12-11 16:23:52 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ~/facets-nf/
+cd ~/project/
 commitID=`git log -n 1 --pretty="%h" -- environment.yml`
 sed -i '/^# environment.yml/d' Singularity && echo -e "\n# environment.yml commit ID: $commitID\n" >> Singularity
 git config --global user.email "voegelec@iarc.fr"
