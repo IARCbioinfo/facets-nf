32721b579fcaddb23ea24e4d9feae85c9c3d6d8b
V-Catherine <voegelec@iarc.fr>
2018-12-11 15:08:37 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git status
 git commit -m "Generated DAG [skip ci]"
 git push origin $CIRCLE_BRANCH
 
-curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/template-nf/trigger/71022421-2df0-4266-9dfa-cacdd9cbf77b/
+curl -H "Content-Type: application/json" --data "{\"source_type\": \"Branch\", \"source_name\": \"$CIRCLE_BRANCH\"}" -X POST https://registry.hub.docker.com/u/iarcbioinfo/facets-nf/trigger/1595f75e-3feb-4bff-9c9d-0232915b1aee/
